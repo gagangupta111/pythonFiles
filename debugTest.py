ecf2522485e9994e0cc87384771d0ffd9309d1d3
gagan gupta <gagan202020@gmail.com>
2025-02-03 23:08:15 +0530

    python files

---
+++ debugTest.py
@@ -0,0 +1,10 @@
+import pdb
+
+def addition(a, b):
+	answer = int(a) * int(b)
+	return answer
+
+x = input("Enter first number : ")
+y = input("Enter second number : ")
+sum = addition(x, y)
+print(sum)
