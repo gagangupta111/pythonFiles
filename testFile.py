32184f4dede6572cf80960fa8e420ab08d20dd48
gagan gupta <gagan202020@gmail.com>
2025-02-04 21:19:01 +0530

    open ai example

---
+++ testFile.py
@@ -0,0 +1,14 @@
+from openai import OpenAI
+
+client = OpenAI(
+  api_key="token")
+
+completion = client.chat.completions.create(
+  model="gpt-4o-mini",
+  store=True,
+  messages=[
+    {"role": "user", "content": "write a haiku about ai"}
+  ]
+)
+
+print(completion.choices[0].message);
